a913486d5c3f2fe3eb2d65bfac6d5a1697e5e3d2
GalinaLuzhetskaya <luzh.galina@yandex.ru>
2024-08-15 07:47:31 +0300

    Add files via upload

---
+++ Peremenniye.py
@@ -0,0 +1,5 @@
+kolichestvo_DZ=12
+kolichestvo_tschasov=1.5
+name_kursa="Python"
+vremia_na_1_zadanie=kolichestvo_tschasov/kolichestvo_DZ
+print('Курс:',name_kursa,',', 'всего задач:',kolichestvo_DZ,',', 'затрачено часов:',kolichestvo_tschasov,',', 'среднее время выполнения:',vremia_na_1_zadanie,'часа.')
